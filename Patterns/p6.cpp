8839ff161fc8a69b562e3dfab9215096cdf37af5
Parmar Suhani <124229698+Suhaniparmar@users.noreply.github.com>
2024-05-29 23:04:29 +0530

    additional patterns added

--- Patterns/p6.cpp
+++ Patterns/p6.cpp
@@ -6,7 +6,7 @@ int main()
     int n=4;
    for(int i=0;i<n;i++)
    {
-       for(int j=1;j<=n-i-1;j++)
+       for(int j=0;j<n-i-1;j++)
        {
            cout<<" ";
        }
@@ -14,7 +14,7 @@ int main()
        {
            cout<<"*";
        }
-       for(int j=1;j<=n-i-1;j++)
+       for(int j=0;j<n-i-1;j++)
        {
            cout<<" ";
        }
--- Patterns/p8.cpp
+++ Patterns/p8.cpp
@@ -1,11 +1,25 @@
-
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    int n=5;
-
+    int n=4;
+   for(int i=0;i<n;i++)
+   {
+       for(int j=1;j<=n-i-1;j++)
+       {
+           cout<<" ";
+       }
+       for(int k=1;k<=((2*i)+1);k++)
+       {
+           cout<<"*";
+       }
+       for(int j=1;j<=n-i-1;j++)
+       {
+           cout<<" ";
+       }
+       cout<<endl;
+   }
    for(int i=0;i<=n;i++)
    {
        for(int j=0;j<i;j++)
